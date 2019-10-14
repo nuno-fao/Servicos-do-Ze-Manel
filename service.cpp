47d822c908c117ee35a91c888f8245fb3c81e2ce
rocas777 <lmiguelapinto@gmail.com>
2019-10-14 22:42:16 +0100

    .

--- service.cpp
+++ service.cpp
@@ -1,1 +1,54 @@
 #include "service.h"
+
+service::service(string origin,string destination,double time,unsigned distance,enum type type, enum state state)
+    : origin(origin),destination(destination), time(time), distance(distance),type(type), state(state)
+{
+    id=lastId++;
+}
+
+string service::getOrigin() const{
+    return origin;
+}
+string service::getDestination() const{
+    return destination;
+}
+double service::getTime() const{
+    return time;
+}
+unsigned service::getDistance() const{
+    return distance;
+}
+type service::getType() const{
+    return type;
+}
+unsigned int service::getId() const{
+    return id;
+}
+state service::getState(){
+    return state;
+}
+
+//set methods
+void service::setOrigin(string origin){
+    this->origin=origin;
+}
+void service::setDestination(string destination){
+
+}
+void service::setTime(double time){
+
+}
+void service::setDistance(unsigned distance){
+
+}
+void service::setType(enum type type){
+
+}
+void service::setState(enum state state){
+
+}
+
+void service::addTruck(truck *truck){
+
+}
+#include "service.h"
--- service.h
+++ service.h
@@ -1,11 +1,56 @@
 #pragma once
+#include <string>
+#include <vector>
+
+#include "truck.h"
+
+using namespace std;
+
+enum type {
+    red, green, blue
+};
+
+enum state {
+    on_queue,onTransit,finished
+};
 
 class Service
 {
 public:
-	Service();
-	~Service();
+    Service(string origin,string destination,double time,unsigned distance,type type, state state);
+    ~Service();
+    static unsigned int lastId;
+
+    //get methods
+    string getOrigin() const;
+    string getDestination() const;
+    double getTime() const;
+    unsigned getDistance() const;
+    type getType() const;
+    unsigned int getId() const;
+    state getState();
+
+    //set methods
+    void setOrigin(string origin);
+    void setDestination(string destination);
+    void setTime(double time);
+    void setDistance(unsigned distance);
+    void setType(type type);
+    void setState(state state);
+
+    void addTruck(Truck *truck);
 
 private:
+    string origin;
+    string destination;
+    double time;
+    unsigned distance;
+    type type;
+    unsigned int id;
+    vector<Truck*> trucks;
+    //static unsigned int lastId;
+    state state;
 
 };
+
+unsigned int Service::lastId=0;
