1f864d0622e0c830f7b0ea5682ec657f7b756993
Nuno Oliveira <oliveiranunopt@hotmail.com>
2019-11-07 15:38:23 +0000

    need to debug now :(

--- truck.cpp
+++ truck.cpp
@@ -377,5 +377,79 @@ void Truck::createTruck(vector<Truck*>* trucks) {
 }
 
 void Truck::removeTruck(vector<Truck*>* trucks) {
+	clearScreen();
+	string license, confirmstr;
+	bool invalidInput;
+	vector<string> auxVec;
 
+	do {
+		invalidInput = false;
+		cout << "What's the license of the new truck (XX-YY-ZZ)? "; getline(cin, license);
+		if (license == "!q") return;
+
+		//verifies if the license is valid or if it already exists.
+		
+		if (license.size() == 6) {
+			auxVec = vectorString(license, "-");
+			if (auxVec.size() == 3) {
+				unsigned short num = 0, letters = 0;
+				for (int i = 0; i != auxVec.size(); i++) {
+					if (strIsNumber(auxVec[i])) {
+						if (stoi(auxVec[i]) < 100 && stoi(auxVec[i]) > 0) {
+							num++;
+						}
+						else {
+							cout << "WRONG FORMAT!!!!\nMust be in XX-YY-ZZ without any other character before or after. 2 pairs of numbers and 1 pair of letters\n";
+							cout << "Your input: " << license << endl;
+							enter_to_exit();
+							invalidInput = true;
+						}
+					}
+					else {
+						if (isalpha(auxVec[0][0]) && isalpha(auxVec[0][1])) {
+							letters++;
+						}
+						else {
+							cout << "WRONG FORMAT!!!!\nMust be in XX-YY-ZZ without any other character before or after. 2 pairs of numbers and 1 pair of letters\n";
+							cout << "Your input: " << license << endl;
+							enter_to_exit();
+							invalidInput = true;
+						}
+					}
+				}
+				if (num == 2 && letters == 1) {
+					for (vector<Truck*>::iterator it = trucks->begin(); it != trucks->end();it++) {
+						if (!((*it)->getregistered()) && (*it)->getavailable()) {
+							if ((*it)->getlicense() == license) {
+								do {
+									clearScreen();
+									cout << "You're about to remove a truck with the following characteristics to this business: " << endl;
+									(*it)->info();
+									cout << "Do you wish proceed (Y/N)? ";
+									cin >> confirmstr;
+									clearBuffer();
+								} while (confirmstr != "Y" && confirmstr != "N" && confirmstr != "y" && confirmstr != "n" && confirmstr != "!q");	//confirmation
+								if (confirmstr == "Y" || confirmstr == "y") {
+									delete (*it);
+									trucks->erase(it);
+								}
+							}
+						}
+						else {
+							cout << "This truck cannot be removed because it is either on transit or registered for a future service" << endl;
+							enter_to_exit();
+						}
+					}
+				}
+				else {
+					cout << "WRONG FORMAT!!!!\nMust be in XX-YY-ZZ without any other character before or after. 2 pairs of numbers and 1 pair of letters\n";
+					cout << "Your input: " << license << endl;
+					enter_to_exit();
+					invalidInput = true;
+				}
+			}
+		}
+	} while (invalidInput);
+
+	
 }
