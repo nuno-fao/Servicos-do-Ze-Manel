893cac5df864043a9e0a7f04ba8fe37fb94f3e32
rocas777 <lmiguelapinto@gmail.com>
2019-12-23 16:58:26 +0000

    added new classes

---
+++ driver.h
@@ -0,0 +1,11 @@
+#ifndef DRIVER_H
+#define DRIVER_H
+
+
+class Driver
+{
+public:
+    Driver();
+};
+
+#endif // DRIVER_H
---
+++ workshop.h
@@ -0,0 +1,11 @@
+#ifndef WORKSHOP_H
+#define WORKSHOP_H
+
+
+class Workshop
+{
+public:
+    Workshop();
+};
+
+#endif // WORKSHOP_H
