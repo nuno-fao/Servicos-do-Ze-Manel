ef71152d580b85a1e26bce860dd98d990589989b
rocas777 <lmiguelapinto@gmail.com>
2020-01-02 21:58:39 +0000

    Update Servicos-do-Ze-Manel.cpp

--- Servicos-do-Ze-Manel.cpp
+++ Servicos-do-Ze-Manel.cpp
@@ -61,6 +61,7 @@ int main()
         Client::loadClients(*Ze_Manel->getVectorClients());
     }
     try {
+        Company::getCompany().getDrivers().resetRoot();
         Driver::loadFromFile();
     } catch (...) {
 
