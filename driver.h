7a5d983b833a69f85872249118243a81463a192c
Nuno Resende <dspninas@gmail.com>
2020-01-03 16:34:58 +0000

    Fiz um bocado da documentação

--- driver.h
+++ driver.h
@@ -14,13 +14,58 @@ public:
     Driver(){
         Driver(1,"0",0);
     }
-    int getNif() const;
-    string getName() const;
-    float getServiceHours() const;
-    bool getDriverActiv() const;
 
+/**
+* @brief Gets the Driver's NIF
+*
+* @return Returns an int representing the driver's NIF
+*/
+    int getNif() const;
+/**
+* @brief Gets the Driver's name
+*
+* @return Returns the driver's name
+*/
+    string getName() const;
+/**
+* @brief Gets the Driver's number of hours of service
+*
+* @return Returns a float representinf the number of hours of service
+*/
+    float getServiceHours() const;
+/**
+* @brief Gets the Driver's status
+*
+* @return Returns true if the driver is active
+*/
+    bool getDriverActiv() const;
+/**
+ * @brief Sets a Driver's NIF
+ *
+ * Updates the Driver's NIF
+ *
+ * @param nif - int contaning NIF to be set
+ * @return Returns nothing
+ */
     void setNif(int nif);
+/**
+* @brief Sets a Driver's name
+*
+* Updates the Driver's name
+*
+* @param name - String containing the Driver's name to be updated
+* @return Returns nothing
+*/
     void setName(string name);
+
+/**
+* @brief Sets a Driver's service hours
+*
+* Updates the Driver's number of hours of service
+*
+* @param ServiceHours - float with number of hours to be set
+* @return Returns nothing
+*/
     void setServiceHours(float ServiceHours);
 
     static void loadFromFile();
@@ -29,10 +74,10 @@ public:
     void toogleDriverActiv();
 
 private:
-    int nif;
-    string name;
-    float service_hours;
-    bool active;
+    int nif; /// Driver's NIF
+    string name; /// Driver's name
+    float service_hours; /// Driver's number of hours of service
+    bool active; /// true if active
 };
 
 bool operator <(const Driver &a,const Driver &b);
